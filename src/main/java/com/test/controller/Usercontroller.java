db5f9fc733aff0e19762bb936a522965101c9aec
wl1990 <1209320511@qq.com>
2017-06-16 16:48:23 +0800

    commit

--- src/main/java/com/test/controller/Usercontroller.java
+++ src/main/java/com/test/controller/Usercontroller.java
@@ -4,8 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
